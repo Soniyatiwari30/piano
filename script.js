a1ea60507ccb02630ffc42eae0492ebcdcab0844
Soniyatiwari30 <tiwarisoniya7777@gmail.com>
2024-09-13 10:59:51 +0530

    new file

---
+++ script.js
@@ -0,0 +1,71 @@
+var white1 = document.getElementById("white1")
+var audio =  new Audio('audio/A.mp3');
+white1.addEventListener("click",function(){
+    audio.play();
+});
+
+var white2 = document.getElementById("white2")
+var audio1 =  new Audio('audio/Ab.mp3');
+white2.addEventListener("click",function(){
+   audio1.play();
+})
+
+var white3 = document.getElementById("white3")
+var audio2 =  new Audio('audio/B.mp3');
+white3.addEventListener("click",function(){
+   audio2.play();
+})
+
+var white4 = document.getElementById("white4")
+var audio3 =  new Audio('audio/Bb.mp3');
+white4.addEventListener("click",function(){
+   audio3.play();
+})
+
+var white5 = document.getElementById("white5")
+var audio4 =  new Audio('audio/C.mp3');
+white5.addEventListener("click",function(){
+   audio4.play();
+})
+
+var white6 = document.getElementById("white6")
+var audio5 =  new Audio('audio/D.mp3');
+white6.addEventListener("click",function(){
+   audio5.play();
+})
+
+var white7 = document.getElementById("white7")
+var audio6 =  new Audio('audio/Db.mp3');
+white7.addEventListener("click",function(){
+   audio6.play();
+})
+
+var black1 = document.getElementById("black1")
+var audio7 =  new Audio('audio/E.mp3');
+black1.addEventListener("click",function(){
+    audio7.play();
+})
+
+var black2 = document.getElementById("black2")
+var audio8 =  new Audio('audio/Eb.mp3');
+black2.addEventListener("click",function(){
+    audio8.play();
+})
+
+var black3 = document.getElementById("black3")
+var audio9 =  new Audio('audio/F.mp3');
+black3.addEventListener("click",function(){
+    audio9.play();
+})
+
+var black4 = document.getElementById("black4")
+var audio10 =  new Audio('audio/G.mp3');
+black4.addEventListener("click",function(){
+    audio10.play();
+})
+
+var black5 = document.getElementById("black5")
+var audio11 =  new Audio('audio/Gb.mp3');
+black5.addEventListener("click",function(){
+    audio11.play();
+})
